6b2dacd2cef75d871e1e0336d3f29d454f26cfce
suriyaprakasamr <suriyaprakasamr@SYNCLAPN17861>
2019-03-06 17:19:11 +0530

    Added logic to pull the repository(ies).

--- CloneReposGitCommand/Program.cs
+++ CloneReposGitCommand/Program.cs
@@ -17,64 +17,79 @@ namespace CloneReposGitCommand
                 return;
             string errorString = string.Empty, outputString = string.Empty;
             string gitInstalledPath = args[0]; //args[0] holds the GIT installed path
+            string reposListPath = args[1]; //args[1] holds txt file which holds the repos list
+            string branchName = args[2]; // //arg[2] -holds the branch name
             if (Directory.Exists(gitInstalledPath))
             {
-                string[] fileNames = Directory.GetFiles(gitInstalledPath);
-                if (fileNames.Contains(gitInstalledPath + "\\git.exe"))
+                try
                 {
-                    if (File.Exists(args[1])) //args[1] holds txt file which holds the repos list.
+                    string[] fileNames = Directory.GetFiles(gitInstalledPath);
+                    if (fileNames.Contains(gitInstalledPath + "\\git.exe"))
                     {
-                        ProcessStartInfo startInfo = new ProcessStartInfo(gitInstalledPath + "\\git.exe")
+                        if (File.Exists(reposListPath))
                         {
-                            CreateNoWindow = false,
-                            RedirectStandardError = true,
-                            RedirectStandardOutput = true,
-                            UseShellExecute = false
-                        };
-                        string[] reposList = File.ReadAllLines(args[1]);
-                        foreach (string repo in reposList)
-                        {
-                            try
+                            ProcessStartInfo startInfo = new ProcessStartInfo(gitInstalledPath + "\\git.exe")
                             {
-                                string cloneCommand = "clone " + repo + " -b " + args[2] + " " + args[3] + "\\" + repo.Substring(repo.LastIndexOf("/") + 1, (repo.LastIndexOf(".") - repo.LastIndexOf("/") - 1)); //arg[2] -holds the branch name; args[3] holds the target folder.
-                                Console.WriteLine("The following command is going to start\n" + cloneCommand);
-                                startInfo.Arguments = cloneCommand;
-                                Process gitProcess = new Process();
-
-                                gitProcess.StartInfo = startInfo;
-                                gitProcess.Start();
-
-                                errorString = gitProcess.StandardError.ReadToEnd();
-                                outputString = gitProcess.StandardOutput.ReadToEnd();
-
-                                gitProcess.WaitForExit();
-                                gitProcess.Close();
-
-                                Console.WriteLine("Successfully executed the command.");
-                            }
-                            catch (Exception e)
+                                CreateNoWindow = false,
+                                RedirectStandardError = true,
+                                RedirectStandardOutput = true,
+                                UseShellExecute = false
+                            };
+                            Console.WriteLine("Enter the action which you need?\n1. Clone\n2. Pull");
+                            int choice = int.Parse(Console.ReadLine());
+                            if (choice == 1 || choice == 2)
                             {
-                                Console.WriteLine("Error in process : " + e.Message + "\nError string:" + errorString);
+                                string[] reposList = File.ReadAllLines(args[1]);
+                                foreach (string repo in reposList)
+                                {
+
+                                    string folderName = args[3] + "\\" + repo.Substring(repo.LastIndexOf("/") + 1, (repo.LastIndexOf(".") - repo.LastIndexOf("/") - 1)); //args[3] holds the target folder.
+                                    string gitCommand = "status";
+                                    if (choice == 1)
+                                        gitCommand = "clone " + repo + " -b " + branchName + " " + folderName;
+                                    else if (choice == 2)
+                                        gitCommand = "--git-dir=" + folderName + "\\.git" + " pull origin " + branchName;
+                                    Console.WriteLine("The following command is going to start\n" + gitCommand);
+                                    startInfo.Arguments = gitCommand;
+                                    Process gitProcess = new Process();
+
+                                    gitProcess.StartInfo = startInfo;
+                                    gitProcess.Start();
+
+                                    errorString = gitProcess.StandardError.ReadToEnd();
+                                    outputString = gitProcess.StandardOutput.ReadToEnd();
+
+                                    gitProcess.WaitForExit();
+                                    gitProcess.Close();
+                                    
+                                    Console.WriteLine(errorString);
+                                    Console.WriteLine(outputString);
+                                }
                             }
-                            finally
+                            else
                             {
-                                Console.WriteLine(outputString);
+                                Console.WriteLine("Entered choice is not valid. No further action.");
                             }
+                                    
                         }
                     }
+                    else
+                    {
+                        Console.WriteLine("GIT.exe not found in this folder or GIT not installed.");
+                    }
                 }
-                else
+                catch (Exception e)
                 {
-                    Console.WriteLine("This folder is not a GIT folder.");
+                    Console.WriteLine("Error in process : " + e.Message);
                 }
 
             }
             else
             {
-                Console.WriteLine("No such folder.");
+                Console.WriteLine("No such folder or not GIT installed.");
             }
-
-            
+            Console.WriteLine("Press a key to exit...");
+            Console.ReadKey(true);
         }
     }
 }
